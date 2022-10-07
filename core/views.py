7d242c638308eb5f0b66ae2916122bbfc7fc64a7
Fabrício Hiury <fabhiury@gmail.com>
2022-10-07 16:52:36 -0300

    Create users

--- core/views.py
+++ core/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Pessoa
 
 def home(request):
-    return render(request, "index.html")
+    pessoas = Pessoa.objects.all()
+    return render(request, "index.html", {"pessoas": pessoas})
+
+def salvar(request):
+    vnome = request.POST.get("nome")
+    Pessoa.objects.create(nome=vnome)
+    pessoas = Pessoa.objects.all()
+    return render(request, "index.html", {"pessoas": pessoas})
